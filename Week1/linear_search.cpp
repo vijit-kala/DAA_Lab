e27f3a70d022b13fa4be3a72add90cbb49173f64
vijit-kala <vijitkala@gmail.com>
2021-07-16 20:18:16 +0530

    Added code for linear search Week1

---
+++ Week1/linear_search.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    freopen("linear_search_in.txt", "r", stdin);
+    freopen("linear_search_out.txt", "w", stdout);
+
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    int testCase;
+    cin >> testCase;
+
+    while (testCase--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        int key;
+        cin >> key;
+        int cntCmp = 0, flag = 0;
+        for (int x : arr) {
+            cntCmp++;
+            if (x == key) {
+                flag = 1;
+                break;
+            }
+        }
+        cout << (flag ? "Present " : "Not Present ") << cntCmp << endl;
+    }
+    return 0;
+}
